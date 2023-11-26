78b9921dfdbcb46a65d3bd440ce99668622af0bb
Jaeseob Hwang <jetsbee@gmail.com>
2023-11-27 03:36:47 +0900

    퀴즈 소요 시간 포맷 개선

--- src/app/(components)/(QuizResult)/QuizResult.tsx
+++ src/app/(components)/(QuizResult)/QuizResult.tsx
@@ -3,6 +3,15 @@ import { useResultReview } from "@/app/(states)/(Client)/(review)/hooks";
 import { notFound } from "next/navigation";
 import { StyledWrapper } from "./QuizResult.styled";
 
+const timeFormat = (milliseconds: number) => {
+  const totalSec = milliseconds / 1000;
+  const roundedTotalSec = Math.round(totalSec);
+  const minStr = Math.floor(totalSec / 60) + "";
+  const secStr = (roundedTotalSec % 60) + "";
+
+  return +minStr !== 0 ? `${minStr}분 ${secStr}초` : `${secStr}초`;
+};
+
 export const QuizResult = ({ id }: { id: number }) => {
   const isClient = useIsClient();
   const resultReview = useResultReview(id);
@@ -18,7 +27,7 @@ export const QuizResult = ({ id }: { id: number }) => {
   return (
     <StyledWrapper>
       <p>퀴즈 결과</p>
-      <p>퀴즈를 맞칠 때까지 소요된 시간: {durationMs}ms</p>
+      <p>퀴즈를 맞칠 때까지 소요된 시간: {timeFormat(durationMs)}</p>
       <p>정답개수: {numOfCorrectAnswer}</p>
       <p>오답 수: {numOfWrongAnswer}</p>
       <p>
